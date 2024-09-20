a892bd4a898b5704bbef9e3f195a07facb851a27
David Fisher <fisherds@rose-hulman.edu>
2024-09-20 13:27:29 -0400

    ready to do sliders

---
+++ colorsliders/lib/color_slider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorSlider extends StatelessWidget {
+  final String label;
+  final double value;
+  final Color color;
+
+  const ColorSlider({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- colorsliders/lib/color_sliders_page.dart
+++ colorsliders/lib/color_sliders_page.dart
@@ -1,3 +1,4 @@
+import 'package:colorsliders/color_slider.dart';
 import 'package:flutter/material.dart';
 
 class ColorSlidersPage extends StatefulWidget {
@@ -8,6 +9,8 @@ class ColorSlidersPage extends StatefulWidget {
 }
 
 class _ColorSlidersPageState extends State<ColorSlidersPage> {
+  double redValue = 0.5, greenValue = 0.0, blueValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +18,25 @@ class _ColorSlidersPageState extends State<ColorSlidersPage> {
         title: const Text("Color Sliders"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Text("Hi"),
+      body: Column(
+        children: [
+          ColorSlider(
+            label: "Red",
+            value: redValue,
+            color: Colors.red,
+          ),
+          ColorSlider(
+            label: "Green",
+            value: greenValue,
+            color: Colors.green,
+          ),
+          ColorSlider(
+            label: "Blue",
+            value: blueValue,
+            color: Colors.blue,
+          ),
+        ],
+      ),
     );
   }
 }
